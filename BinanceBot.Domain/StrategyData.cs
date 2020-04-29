1867c9e75adfd455229049781e5bf29340cbe78d
Chaitra Dangat <chaitradangat084@gmail.com>
2020-04-30 03:43:02 +0530

    new classes to consolidate the strategy variables and increase code readbility

---
+++ BinanceBot.Domain/StrategyData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BinanceBot.Domain
+{
+    /// <summary>
+    /// Class to hold all data related to a strategy
+    /// </summary>
+    public class StrategyData
+    {
+        //currentClose, risk, reward, leverage,signalStrength, decreaseOnNegative,ohlckandles
+        //ref currentPostion,ref strategyOutput
+
+        public StrategyData()
+        {
+            this.isBuy = default(bool);
+
+            this.isSell = default(bool);
+
+            this.mood = default(string);
+
+            this.trend = default(string);
+
+            this.shortPercentage = default(decimal);
+
+            this.longPercentage = default(decimal);
+
+            this.histdata = default(string);
+        }
+
+
+        public bool isBuy { get; set; }
+
+        public bool isSell { get; set; }
+
+        public string trend { get; set; }
+
+        public string mood { get; set; }
+
+        public string histdata { get; set; }
+
+        public decimal shortPercentage { get; set; }
+
+        public decimal longPercentage { get; set; }
+
+        public decimal profitFactor { get; set; }
+    }
+}
---
+++ BinanceBot.Domain/StrategyInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinanceBot.Domain
+{
+    /// <summary>
+    /// Class to accomodate all inputs for the strategy
+    /// </summary>
+    public class StrategyInput
+    {
+        public decimal decreaseOnNegative { get; set; }
+
+        public int candleCount { get; set; }
+
+        public string timeframe { get; set; }
+
+        public decimal reward { get; set; }
+
+        public decimal risk { get; set; }
+
+        public decimal leverage { get; set; }
+
+        public string symbol { get; set; }
+
+        public int signalStrength { get; set; }
+
+        public decimal quantity { get; set; }
+
+        public decimal currentClose { get; set; }
+    }
+}
