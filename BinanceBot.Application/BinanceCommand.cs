00f70f6adce2f37d19de0e2af5dde7a784534b8a
Chaitra Dangat <chaitradangat084@gmail.com>
2020-05-08 20:48:44 +0530

    -code cleanup- removed unused code

--- BinanceBot.Application/BinanceCommand.cs
+++ BinanceBot.Application/BinanceCommand.cs
@@ -32,107 +32,6 @@ namespace BinanceBot.Application
             pingtime = BinanceBotSettings.settings.PingTimer;
         }
 
-        public void ConnectFuturesBot(string symbol, decimal quantity, decimal risk, decimal reward, decimal leverage, int signalStrength, string timeframe, int candleCount, bool isLive, decimal decreaseOnNegative)
-        {
-            #region -strategy and function level variables-
-            var openclosestrategy = new OpenCloseStrategy();
-
-            var profitFactor = (decimal)1;
-
-            var errorCount = 0;
-
-            webCall.AssignBinanceWebCallFeatures(symbol); //improve this further later
-            #endregion
-
-            using (webCall.client = new BinanceClient())
-            {
-                while (true)
-                {
-                    try
-                    {
-                        #region -variables refreshed every cycle-
-
-                        Stopwatch sw = new Stopwatch();
-
-                        sw.Start();
-
-                        var isBuy = default(bool);
-
-                        var isSell = default(bool);
-
-                        var mood = default(string);
-
-                        var trend = default(string);
-
-                        var shortPercentage = default(decimal);
-
-                        var longPercentage = default(decimal);
-
-                        List<OHLCKandle> ohlckandles = new List<OHLCKandle>();
-
-                        var currentClose = default(decimal);
-
-                        var currentPosition
-
-                         = new SimplePosition
-                         {
-                             PositionID = -1,
-
-                             PositionType = "",
-
-                             EntryPrice = -1,
-
-                             Quantity = quantity,
-
-                             Trend = "",
-
-                             Mood = ""
-                         };
-
-                        var histdata = default(string);
-
-                        var strategyOutput = StrategyOutput.None;
-
-                        Thread.Sleep(pingtime);
-                        #endregion
-
-                        if (isLive)
-                        {
-                            webCall.GetCurrentPosition(ref currentPosition, quantity, ref profitFactor);
-                        }
-
-                        webCall.GetKLinesDataCached(timeframe, candleCount, ref currentClose, ref ohlckandles);
-
-                        openclosestrategy.RunStrategy(ohlckandles, ref isBuy, ref isSell, ref trend, ref mood,
-                        ref histdata, ref currentPosition, currentClose, risk, reward, leverage, ref shortPercentage,
-                        ref longPercentage, ref profitFactor, signalStrength, ref strategyOutput, decreaseOnNegative);
-
-                        if (isLive && strategyOutput != StrategyOutput.None)
-                        {
-                            //PlaceOrders(quantity, currentClose, strategyOutput, longPercentage, shortPercentage); old logic broken here change laterz
-                        }
-
-                        sw.Stop();
-
-                        DumpToConsole(isBuy, isSell, mood, trend, currentClose, currentPosition, shortPercentage,
-                        longPercentage, reward, risk, profitFactor, leverage,
-                        symbol, sw.ElapsedMilliseconds, signalStrength, histdata, openclosestrategy.BuyCounter, openclosestrategy.SellCounter);
-                    }
-                    catch (Exception)
-                    {
-                        Thread.Sleep(10);
-
-                        ++errorCount;
-                    }
-
-                    if (errorCount >= 300)
-                    {
-                        break;
-                    }
-                }
-            }
-        }
-
         public void StartRoBot(RobotInput robotInput, bool isLive)
         {
             #region -strategy and function level variables-
@@ -205,7 +104,6 @@ namespace BinanceBot.Application
             }
         }
 
-
         public void PlaceOrders(decimal quantity, decimal currrentClose, StrategyOutput strategyOutput, StrategyData strategyData)
         {
             BinancePlacedOrder placedOrder = null;
@@ -258,100 +156,6 @@ namespace BinanceBot.Application
             }
 
         }
-
-        #region -dump code-
-        private void DumpToConsole(bool isBuy, bool isSell, string mood, string trend, decimal currentClose, SimplePosition order, decimal shortPercentage, decimal longPercentage, decimal reward, decimal risk, decimal profitFactor, decimal leverage, string symbol, long
-            cycleTime, int signalStrength, string histdata, int BuyCounter, int SellCounter)
-        {
-            Console.Clear();
-
-            Console.WriteLine();
-
-            Console.WriteLine("\n--------------------------------------------------------------------------");
-
-            Console.WriteLine("\nMARKET DETAILS: \n");
-
-            //latest price
-            Console.WriteLine("{0} : {1}\n", symbol, currentClose);
-
-            //mood
-            if (mood == "BULLISH")
-            {//\u02C4
-                Console.WriteLine("MOOD    : {0}\n", "UP");
-            }
-            else if (mood == "BEARISH")
-            {
-                Console.WriteLine("MOOD    : {0}\n", "DOWN");
-            }
-            else
-            {
-                Console.WriteLine("MOOD : {0}\n", "");
-            }
-
-            //trend
-            if (trend == "BULLISH")
-            {
-                Console.WriteLine("TREND   : {0}\n", "UP");
-            }
-            else if (trend == "BEARISH")
-            {
-                Console.WriteLine("TREND   : {0}\n", "DOWN");
-            }
-            else
-            {
-                Console.WriteLine("TREND : {0}\n", "");
-            }
-
-            //signal
-            if (isBuy)
-            {
-                Console.WriteLine("SIGNAL  : {0}\n", "BUY");
-                Console.WriteLine("SIGNA%  : {0}%\n", 100 * BuyCounter / signalStrength);
-            }
-            else if (isSell)
-            {
-                Console.WriteLine("SIGNAL  : {0}\n", "SELL");
-                Console.WriteLine("SIGNA%  : {0}%\n", 100 * SellCounter / signalStrength);
-            }
-            else
-            {
-                Console.WriteLine("SIGNAL  : {0}\n", "NO SIGNAL");
-            }
-
-            Console.WriteLine("HISTORY : {0}", histdata);
-
-            Console.WriteLine("\n--------------------------------------------------------------------------");
-
-            Console.WriteLine("\nORDER DETAILS: \n");
-
-            Console.WriteLine("ID {0}\n", order?.PositionID);
-
-            Console.WriteLine("TYPE {0} \n", order?.PositionType);
-
-            Console.WriteLine("ENTRY PRICE {0} \n", order?.EntryPrice);
-
-            if (order?.PositionID != -1 && order?.PositionType == "SELL")
-            {
-                Console.WriteLine("PERCENTAGE {0} \n", Math.Round(shortPercentage, 3));
-            }
-            if (order?.PositionID != -1 && order?.PositionType == "BUY")
-            {
-                Console.WriteLine("PERCENTAGE {0} \n", Math.Round(longPercentage, 3));
-            }
-
-            Console.WriteLine("ADJUSTED PROFIT LIMIT {0}% \n", reward * profitFactor);
-
-            Console.WriteLine("CURRENT PROFIT LIMIT {0}% \n", reward);
-
-            Console.WriteLine("CURRENT LOSS LIMIT {0}% \n", risk);
-
-            Console.WriteLine("CURRENT LEVERAGE {0}x\n", leverage);
-
-            Console.WriteLine("--------------------------------------------------------------------------\n");
-
-            Console.WriteLine("Refresh Rate {0} milliseconds\n", cycleTime);
-        }
-
 
         private void DumpToConsole(StrategyData strategyData, SimplePosition order, RobotInput sInput, decimal currentClose, long cycleTime, int BuyCounter, int SellCounter)
         {
@@ -467,6 +271,6 @@ namespace BinanceBot.Application
 
             File.AppendAllLines("debug.logs", new[] { debuginfo });
         }
-        #endregion
+        
     }
 }
