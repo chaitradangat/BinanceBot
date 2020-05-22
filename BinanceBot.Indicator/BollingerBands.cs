23e882826ff523879b3184878b1c4661ebbe6f0b
Chaitra Dangat <chaitradangat084@gmail.com>
2020-05-23 02:25:09 +0530

    added indicator - bollinger bands

---
+++ BinanceBot.Indicator/BollingerBands.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using BinanceBot.Domain;
+
+using PineScriptPort;
+
+
+namespace BinanceBot.Indicator
+{
+    /// <summary>
+    /// The BollingerBand Data Indicator
+    /// </summary>
+    public class BollingerBands
+    {
+        private List<OHLCKandle> BollingerData;
+
+        public decimal BollingerUpper { get; set; }
+
+        public decimal BollingerMiddle { get; set; }
+
+        public decimal BollingerLower { get; set; }
+
+        public bool BollTopCrossed { get; set; }
+
+        public bool BollBottomCrossed { get; set; }
+
+        public bool BollMiddleCrossed { get; set; }
+
+        public BollingerBands(List<OHLCKandle> kandles, int bollCrossDistance = 7)
+        {
+            //make a copy to avoid spoiling the inputdata
+            var kcopy = kandles.Select(x => new OHLCKandle
+            {
+                Close = x.Close,
+                CloseTime = x.CloseTime,
+                Open = x.Open,
+                OpenTime = x.OpenTime,
+                High = x.High,
+                Low = x.Low
+            }).ToList();
+
+            Calculate(kandles, bollCrossDistance);
+        }
+
+        /// <summary>
+        /// Computes Bollinger Bands
+        /// </summary>
+        /// <param name="kandles">The input kandles over which bollinger bands will be calculated</param>
+        /// <param name="bollCrossDistance">The maximum distance till which cross with lower and upper sma is valid</param>
+        public void Calculate(List<OHLCKandle> kandles, int bollCrossDistance)
+        {
+            PineScriptFunction fn = new PineScriptFunction();
+
+            var openseries = kandles.Select(x => (decimal)x.Open).ToList();
+
+            var closeseries = kandles.Select(x => (decimal)x.Close).ToList();
+
+            this.BollingerData = fn.bollinger(kandles, 20);
+
+            this.BollingerUpper = BollingerData.Last().High;
+
+            this.BollingerMiddle = BollingerData.Last().Close;
+
+            this.BollingerLower = BollingerData.Last().Low;
+
+            var pricecrosstopband = fn.crossunder(openseries, BollingerData.Select(x => x.High).ToList());
+
+            var pricecrossbottomband = fn.crossover(closeseries, BollingerData.Select(x => x.Low).ToList());
+
+            var pricecrossmiddleband1 = fn.crossunder(openseries, BollingerData.Select(x => x.Close).ToList());
+
+            var pricecrossmiddleband2 = fn.crossover(closeseries, BollingerData.Select(x => x.Close).ToList());
+
+            this.BollTopCrossed = pricecrosstopband.Skip(pricecrosstopband.Count - bollCrossDistance).Take(bollCrossDistance).Contains(true);
+
+            this.BollBottomCrossed = pricecrossbottomband.Skip(pricecrossbottomband.Count - bollCrossDistance).Take(bollCrossDistance).Contains(true);
+
+            this.BollMiddleCrossed = pricecrossmiddleband1.Skip(pricecrossmiddleband1.Count - 2).Take(2).Contains(true)||
+                                     pricecrossmiddleband2.Skip(pricecrossmiddleband2.Count - 2).Take(2).Contains(true);
+        }
+
+
+
+
+    }
+
+
+
+
+}
