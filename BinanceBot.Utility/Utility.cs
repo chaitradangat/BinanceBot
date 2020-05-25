148172b45c863fae04d3a92d497fbced62e85f2e
Chaitra Dangat <chaitradangat084@gmail.com>
2020-05-25 20:13:30 +0530

    Utility Project which will contain common functions used across the solution

---
+++ BinanceBot.Utility/Utility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using System.IO;
+
+namespace BinanceBot.Utility
+{
+    public static class Utility
+    {
+        public static void EnableLogging()
+        { 
+        
+        }
+    }
+}
