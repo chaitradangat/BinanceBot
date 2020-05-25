7d628a821613b62f390ef46b7326a2766c8f64de
Chaitra Dangat <chaitradangat084@gmail.com>
2020-05-25 20:17:03 +0530

    the column header

--- BinanceBot.Utility/Utility.cs
+++ BinanceBot.Utility/Utility.cs
@@ -6,9 +6,15 @@ namespace BinanceBot.Utility
 {
     public static class Utility
     {
+        /// <summary>
+        /// Function to enable logging and write initial schema
+        /// </summary>
         public static void EnableLogging()
-        { 
-        
+        {
+            if (!File.Exists("debug.logs"))
+            {
+                File.AppendAllLines("debug.logs", new[] { "Date	Signal\tPrice\t%\tSignalHistory\tBU\tBM\tBL\tS0\tS1\tA1\tA2\tA3\tA4" });
+            }
         }
     }
 }
