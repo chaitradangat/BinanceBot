4908d48d0c8929093a70a9c72d30285411a46efd
Chaitra Dangat <chaitradangat084@gmail.com>
2020-05-30 21:19:32 +0530

    added function to display to console,renamed namespace to common

--- BinanceBot.Utility/Utility.cs
+++ BinanceBot.Utility/Utility.cs
@@ -6,7 +6,7 @@ using BinanceBot.Domain;
 
 using BinanceBot.Settings;
 
-namespace BinanceBot.Utility
+namespace BinanceBot.Common
 {
     public static class Utility
     {
@@ -78,6 +78,119 @@ namespace BinanceBot.Utility
             File.AppendAllLines(PrimaryLogPath, new[] { debuginfo });
 
             File.AppendAllLines(SecondaryLogPath, new[] { debuginfo });
+        }
+
+        public static void DumpToConsole(StrategyData strategyData, SimplePosition order, RobotInput robotInput, decimal BollingerFactor, string LastAvoidReason, long cycleTime)
+        {
+            Console.Clear();
+
+            Console.WriteLine("\n\n--------------------------------------------------------------------------");
+
+            Console.WriteLine("\nMARKET DETAILS: \n");
+
+            //latest price
+            Console.WriteLine("{0} : {1} \n", robotInput.symbol, strategyData.currentClose);
+
+            Console.WriteLine("BBAND   : {0}%   {1}%   {2}%  {3}{4}{5}\n",
+                strategyData.BollingerUpperPercentage,
+                strategyData.BollingerMiddlePercentage,
+                strategyData.BollingerLowerPercentage,
+                strategyData.BollTopCrossed ? "*TOPCROSSED*" : "",
+                strategyData.BollBottomCrossed ? "*BOTTOMCROSSED*" : "",
+                strategyData.BollMiddleCrossed ? "*MIDDLECROSSED*" : ""
+                );
+
+            //mood
+            if (strategyData.mood == "BULLISH")
+            {
+                Console.Write("MOOD    : [{0}] ", "UP");
+            }
+            else if (strategyData.mood == "BEARISH")
+            {
+                Console.Write("MOOD    : [{0}] ", "DOWN");
+            }
+            else
+            {
+                Console.Write("MOOD : [{0}] ", "");
+            }
+
+            //trend
+            if (strategyData.trend == "BULLISH")
+            {
+                Console.WriteLine("  TREND  : [{0}]\n", "UP");
+            }
+            else if (strategyData.trend == "BEARISH")
+            {
+                Console.WriteLine("  TREND  : [{0}]\n", "DOWN");
+            }
+            else
+            {
+                Console.WriteLine("TREND : {0}\n", "");
+            }
+
+            if (strategyData.PrevDecision.ToString().ToLower().Contains("buy") && strategyData.LatestSignalStrength != 0)
+            {
+                //signal
+                Console.WriteLine("DECISION : {0}  {1}%  @STRENGTH OF {2}\n", strategyData.PrevDecision.ToString(), 100 * strategyData.BuyCounter / strategyData.LatestSignalStrength, strategyData.LatestSignalStrength);
+            }
+            else if (strategyData.PrevDecision.ToString().ToLower().Contains("sell") && strategyData.LatestSignalStrength != 0)
+            {
+                Console.WriteLine("DECISION : {0}  {1}%  @STRENGTH OF {2}\n", strategyData.PrevDecision.ToString(), 100 * strategyData.SellCounter / strategyData.LatestSignalStrength, strategyData.LatestSignalStrength);
+            }
+            else
+            {
+                Console.WriteLine("DECISION : {0}\n", "NO DECISION");
+            }
+
+            Console.WriteLine("SGNLHISTORY : {0}\n", strategyData.histdata);
+
+            if (strategyData.AvoidReasons != null && strategyData.AvoidReasons.Count > 0)
+            {
+                LastAvoidReason = "";
+                foreach (var AvoidReason in strategyData.AvoidReasons)
+                {
+                    LastAvoidReason += AvoidReason.ToString() + "\t";
+                }
+            }
+
+            Console.WriteLine("SKIPHISTORY : {0}", LastAvoidReason);
+
+            Console.WriteLine("\n--------------------------------------------------------------------------");
+
+            Console.WriteLine("\nORDER DETAILS: \n");
+
+            Console.WriteLine("ID {0}\n", order?.PositionID);
+
+            Console.WriteLine("TYPE {0} \n", order?.PositionType);
+
+            Console.WriteLine("ENTRY PRICE {0} \n", order?.EntryPrice);
+
+            if (order?.PositionID != -1 && order?.PositionType == "SELL")
+            {
+                Console.WriteLine("PERCENTAGE {0} \n", Math.Round(strategyData.shortPercentage, 3));
+            }
+            if (order?.PositionID != -1 && order?.PositionType == "BUY")
+            {
+                Console.WriteLine("PERCENTAGE {0} \n", Math.Round(strategyData.longPercentage, 3));
+            }
+
+
+
+            Console.WriteLine("LIMITS > ADJPROFIT *{0}%*  PROFIT *{1}%*  LOSS *{2}%* BOLL *{3}%*\n",
+            Math.Round(robotInput.reward * strategyData.profitFactor, 3),
+            robotInput.reward,
+            robotInput.risk,
+            Math.Round(robotInput.reward * BollingerFactor, 3));
+
+            Console.WriteLine("LEVERAGE {0}x\n", robotInput.leverage);
+
+            Console.WriteLine("--------------------------------------------------------------------------\n");
+
+            Console.WriteLine("Refresh Rate {0} milliseconds\n", cycleTime);
+
+
+
+
         }
 
     }
